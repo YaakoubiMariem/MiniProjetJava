0952cb5295262c3459d6ec853192efeab3d71e87
YaakoubiMariem <mariem.yaakoubi@etudiant-enit.utm.tn>
2025-05-14 16:59:59 +0100

    Update PretraiteurIdentite.java

--- src/mini_projet/PretraiteurIdentite.java
+++ src/mini_projet/PretraiteurIdentite.java
@@ -1,10 +1,7 @@
 package mini_projet;
 import java.util.List;
 
-<<<<<<< HEAD
-=======
 
->>>>>>> branch 'master' of https://github.com/YaakoubiMariem/MiniProjetJava.git
 public class PretraiteurIdentite implements Pretraiteur {
 
     public List<String> traiter(List<String> chaines) {
