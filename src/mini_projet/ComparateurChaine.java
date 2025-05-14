15c69c8cd5a0825ddcbc32aa5122aaf500034e44
YaakoubiMariem <mariem.yaakoubi@etudiant-enit.utm.tn>
2025-05-14 16:54:11 +0100

    Update ComparateurChaine.java

--- src/mini_projet/ComparateurChaine.java
+++ src/mini_projet/ComparateurChaine.java
@@ -4,8 +4,5 @@ public interface ComparateurChaine {
     double comparer(String s1, String s2);
     TypeMesure getType();
 
-<<<<<<< HEAD
-=======
 
->>>>>>> branch 'master' of https://github.com/YaakoubiMariem/MiniProjetJava.git
 }
