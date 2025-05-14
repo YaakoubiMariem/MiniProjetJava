106d96b3d9c95427527a420b55043ad7d7ef25e8
YaakoubiMariem <mariem.yaakoubi@etudiant-enit.utm.tn>
2025-05-14 17:04:00 +0100

    Update MainPretraiteurDecomposition.java

--- src/mini_projet/MainPretraiteurDecomposition.java
+++ src/mini_projet/MainPretraiteurDecomposition.java
@@ -1,24 +1,12 @@
 package mini_projet;
-<<<<<<< HEAD
 import java.util.List;
+
 
 public class MainPretraiteurDecomposition {
     public static void main(String[] args) {
         Pretraiteur pretraiteur = new PretraiteurDecomposition();
-
-        List<String> entree = List.of("Mouhamed  Yassine Yaakoubi");
-=======
-
-import java.util.List;
-
-public class MainPretraiteurDecomposition {
-    public static void main(String[] args) {
-        Pretraiteur pretraiteur = new PretraiteurDecomposition();
-
         List<String> entree = List.of("Mouhamed Yassine Yaakoubi");
->>>>>>> branch 'master' of https://github.com/YaakoubiMariem/MiniProjetJava.git
         List<String> sortie = pretraiteur.traiter(entree);
-
         System.out.println("Entrée : " + entree);
         System.out.println("Sortie (découpée) : " + sortie);
     }
