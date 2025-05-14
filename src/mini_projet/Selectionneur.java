ff48c07e1d35c8682f13b3be690b0720da61e53e
YaakoubiMariem <mariem.yaakoubi@etudiant-enit.utm.tn>
2025-05-14 16:58:17 +0100

    Update Selectionneur.java

--- src/mini_projet/Selectionneur.java
+++ src/mini_projet/Selectionneur.java
@@ -1,10 +1,6 @@
 package mini_projet;
 import java.util.List;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> branch 'master' of https://github.com/YaakoubiMariem/MiniProjetJava.git
 public interface Selectionneur {
 	List<CoupleNomsAvecScore> selectionner(List<CoupleNomsAvecScore> resultats);
 
