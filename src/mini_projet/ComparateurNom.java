635d16441640cf22ddc2863689037e21325ad0d9
YaakoubiMariem <mariem.yaakoubi@etudiant-enit.utm.tn>
2025-05-14 16:55:04 +0100

    Update ComparateurNom.java

--- src/mini_projet/ComparateurNom.java
+++ src/mini_projet/ComparateurNom.java
@@ -1,10 +1,5 @@
 package mini_projet;
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> branch 'master' of https://github.com/YaakoubiMariem/MiniProjetJava.git
 public interface ComparateurNom {
 	double comparer(Nom n1, Nom n2);
 
