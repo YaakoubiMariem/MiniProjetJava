35af1ff95cd6027a395957a228d1d99650593a86
YaakoubiMariem <mariem.yaakoubi@etudiant-enit.utm.tn>
2025-05-14 17:00:15 +0100

    Update PretraiteurDecomposition.java

--- src/mini_projet/PretraiteurDecomposition.java
+++ src/mini_projet/PretraiteurDecomposition.java
@@ -1,8 +1,5 @@
 package mini_projet;
-<<<<<<< HEAD
-=======
 
->>>>>>> branch 'master' of https://github.com/YaakoubiMariem/MiniProjetJava.git
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
