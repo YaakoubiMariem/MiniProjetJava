24a39839ad9f83c6f7a8e77a1f41917f65bac01c
YaakoubiMariem <mariem.yaakoubi@etudiant-enit.utm.tn>
2025-05-14 16:55:49 +0100

    Update ComparateurNomEquals.java

--- src/mini_projet/ComparateurNomEquals.java
+++ src/mini_projet/ComparateurNomEquals.java
@@ -1,8 +1,5 @@
 package mini_projet;
-<<<<<<< HEAD
-=======
 
->>>>>>> branch 'master' of https://github.com/YaakoubiMariem/MiniProjetJava.git
 import java.util.List;
 
 public class ComparateurNomEquals implements ComparateurNom {
