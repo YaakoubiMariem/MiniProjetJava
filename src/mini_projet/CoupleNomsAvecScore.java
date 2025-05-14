d0d88671ed71ba24806fb28c5d92b98bc66e2d9e
YaakoubiMariem <mariem.yaakoubi@etudiant-enit.utm.tn>
2025-05-14 17:11:29 +0100

    Update CoupleNomsAvecScore.java

--- src/mini_projet/CoupleNomsAvecScore.java
+++ src/mini_projet/CoupleNomsAvecScore.java
@@ -1,9 +1,5 @@
 package mini_projet;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> branch 'master' of https://github.com/YaakoubiMariem/MiniProjetJava.git
 public class CoupleNomsAvecScore {
     private final double score;
     private final Nom nom1;
