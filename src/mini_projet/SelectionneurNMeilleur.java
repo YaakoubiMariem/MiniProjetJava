5f3bb3ecf75c4893a13e0fc4d2bf86dc2db0447a
YaakoubiMariem <mariem.yaakoubi@etudiant-enit.utm.tn>
2025-05-14 16:57:58 +0100

    Update SelectionneurNMeilleur.java

--- src/mini_projet/SelectionneurNMeilleur.java
+++ src/mini_projet/SelectionneurNMeilleur.java
@@ -1,8 +1,5 @@
 package mini_projet;
-<<<<<<< HEAD
-=======
 
->>>>>>> branch 'master' of https://github.com/YaakoubiMariem/MiniProjetJava.git
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
