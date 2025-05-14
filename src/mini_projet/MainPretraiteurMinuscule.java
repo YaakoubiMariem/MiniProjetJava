df53527ddd5a4b3a9e908c23f3c7678430a6e165
YaakoubiMariem <mariem.yaakoubi@etudiant-enit.utm.tn>
2025-05-14 17:03:12 +0100

    Update MainPretraiteurMinuscule.java

--- src/mini_projet/MainPretraiteurMinuscule.java
+++ src/mini_projet/MainPretraiteurMinuscule.java
@@ -1,23 +1,12 @@
 package mini_projet;
-<<<<<<< HEAD
 import java.util.List;
 
-public class MainPretraiteurMinuscule {
-    public static void main(String[] args) {
-        Pretraiteur pretraiteur = new PretraiteurMinuscule();
-
-        List<String> entree = List.of("Mariem YAAkoubI","ဦးေဇတ္တြန္း ");
-=======
-
-
-import java.util.List;
 
 public class MainPretraiteurMinuscule {
     public static void main(String[] args) {
         Pretraiteur pretraiteur = new PretraiteurMinuscule();
 
         List<String> entree = List.of("Mariem YAAkoubI");
->>>>>>> branch 'master' of https://github.com/YaakoubiMariem/MiniProjetJava.git
         List<String> sortie = pretraiteur.traiter(entree);
 
         System.out.println("Entrée : " + entree);
