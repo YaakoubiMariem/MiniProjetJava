dded3bc4d7a4355978e2d4caceb00367164cffb2
YaakoubiMariem <mariem.yaakoubi@etudiant-enit.utm.tn>
2025-05-14 17:02:44 +0100

    Update MainPretraiteurSansAccents.java

--- src/mini_projet/MainPretraiteurSansAccents.java
+++ src/mini_projet/MainPretraiteurSansAccents.java
@@ -1,24 +1,11 @@
 package mini_projet;
-<<<<<<< HEAD
 import java.util.List;
 
 public class MainPretraiteurSansAccents {
     public static void main(String[] args) {
         Pretraiteur pretraiteur = new PretraiteurSansAccents();
-
-        List<String> entree = List.of("René Prévert", "Hélène Müller", "François Cédille","ÃoldaÅŸ HallyÃ½ewiÃ§ Åžeripow","ဦးေဇတ္တြန္း ");
-=======
-
-import java.util.List;
-
-public class MainPretraiteurSansAccents {
-    public static void main(String[] args) {
-        Pretraiteur pretraiteur = new PretraiteurSansAccents();
-
         List<String> entree = List.of("René Prévert", "Hélène Müller", "François Cédille");
->>>>>>> branch 'master' of https://github.com/YaakoubiMariem/MiniProjetJava.git
         List<String> sortie = pretraiteur.traiter(entree);
-
         System.out.println("Entrée : " + entree);
         System.out.println("Sortie (sans accents) : " + sortie);
     }
