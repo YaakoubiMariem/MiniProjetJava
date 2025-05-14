ad160886f9b47f1791e977196c44026110a4e0e5
YaakoubiMariem <mariem.yaakoubi@etudiant-enit.utm.tn>
2025-05-14 17:07:10 +0100

    Update MainGenerateurParMotsCommunsAvecIndex.java

--- src/mini_projet/MainGenerateurParMotsCommunsAvecIndex.java
+++ src/mini_projet/MainGenerateurParMotsCommunsAvecIndex.java
@@ -1,8 +1,4 @@
 package mini_projet;
-<<<<<<< HEAD
-=======
-
->>>>>>> branch 'master' of https://github.com/YaakoubiMariem/MiniProjetJava.git
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
