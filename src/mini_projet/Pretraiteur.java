5a839385f1a3930564f52cca98e7d0d11df7b9fd
YaakoubiMariem <mariem.yaakoubi@etudiant-enit.utm.tn>
2025-05-14 17:00:33 +0100

    Update Pretraiteur.java

--- src/mini_projet/Pretraiteur.java
+++ src/mini_projet/Pretraiteur.java
@@ -3,10 +3,7 @@ import java.util.List;
 
 public interface Pretraiteur {
 	List<String> traiter(List<String> chaines);
-<<<<<<< HEAD
-=======
 
->>>>>>> branch 'master' of https://github.com/YaakoubiMariem/MiniProjetJava.git
 }
 
 
