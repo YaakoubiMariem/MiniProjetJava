3c3935f2101d194705f65cc161e5ab423bd03e71
YaakoubiMariem <mariem.yaakoubi@etudiant-enit.utm.tn>
2025-05-14 16:56:52 +0100

    Update TypeMesure.java

--- src/mini_projet/TypeMesure.java
+++ src/mini_projet/TypeMesure.java
@@ -2,8 +2,5 @@ package mini_projet;
 
 public enum TypeMesure {
 	SIMILARITE, DISTANCE
-<<<<<<< HEAD
-	
-=======
->>>>>>> branch 'master' of https://github.com/YaakoubiMariem/MiniProjetJava.git
+
 }
